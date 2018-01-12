45ba974dcc214d303acb71d5ef7dd0c180669ef8
Matthieu Stadelmann <matthstadelmann@gmail.com>
2018-01-12 18:58:15 +0100

    new comit

--- main.js
+++ main.js
@@ -12,27 +12,7 @@ app.get('/', function(req, res) {
 
 app.use(express.static('./public'));
 
-const {GoogleAuth} = require('google-auth-library');
 
-// load the environment variable with our keys
-const keysEnvVar = process.env['CREDS'];
-if (!keysEnvVar) {
-  throw new Error('The $CREDS environment variable was not found!');
-}
-const keys = JSON.parse(keysEnvVar);
-
-async function main() {
-  const auth = new GoogleAuth();
-  // load the JWT or UserRefreshClient from the keys
-  const client = auth.fromJSON(keys);
-  client.scopes = ['https://www.googleapis.com/auth/cloud-platform'];
-  await client.authorize();
-  const url = `https://www.googleapis.com/dns/v1/projects/${keys.project_id}`;
-  const res = await client.request({url});
-  console.log(res.data);
-}
-
-main().catch(console.error);
 
 io.on('connection', function(socket) {
 
