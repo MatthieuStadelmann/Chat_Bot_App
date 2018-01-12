8c0ce9dfecda2fb9e5867372783299282258390e
Matthieu Stadelmann <matthstadelmann@gmail.com>
2018-01-12 17:05:41 +0100

    chatBot setup done

---
+++ api.js
@@ -0,0 +1,43 @@
+function dialogFlow(query) {
+
+  // You can find your project ID in your Dialogflow agent settings
+  const projectId = 'grover-b350e'; //https://dialogflow.com/docs/agents#settings
+  const sessionId = 'quickstart-session-id';
+  const languageCode = 'en-US';
+
+  // Instantiate a DialogFlow client.
+  const dialogflow = require('dialogflow');
+  const sessionClient = new dialogflow.SessionsClient();
+
+  // Define session path
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+        languageCode: languageCode
+      }
+    }
+  };
+  // Send request and log result
+  return sessionClient.detectIntent(request).then(responses => {
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+    return result.fulfillmentText
+
+}).catch(err => {
+  console.error('ERROR:', err);
+});
+};
+
+exports.dialogFlow = dialogFlow;
---
+++ main.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const http = require('http');
+const app = express()
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+const ai = require('./api');
+const dialogFlow = ai.dialogFlow;
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static('./public'));
+
+io.on('connection', function(socket) {
+
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+    dialogFlow(msg).then((resp) => {
+      io.emit('chat message', resp);
+    });
+  });
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => console.log('listening on port 3000!'))
