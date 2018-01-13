2b0a77752fcfa79642107429c830cfc24c2ff7e7
Matthieu Stadelmann <matthstadelmann@gmail.com>
2018-01-13 17:47:13 +0100

    Added Redis

--- api.js
+++ api.js
@@ -1,20 +1,14 @@
 function dialogFlow(query) {
 
-
-
-  // You can find your project ID in your Dialogflow agent settings
   const projectId = 'grover-b350e'; //https://dialogflow.com/docs/agents#settings
   const sessionId = 'quickstart-session-id';
   const languageCode = 'en-US';
 
-  // Instantiate a DialogFlow client.
   const dialogflow = require('dialogflow');
   const sessionClient = new dialogflow.SessionsClient();
 
-  // Define session path
   const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-  // The text query request.
   const request = {
     session: sessionPath,
     queryInput: {
@@ -24,7 +18,6 @@ function dialogFlow(query) {
       }
     }
   };
-  // Send request and log result
   return sessionClient.detectIntent(request).then(responses => {
     console.log('Detected intent');
     const result = responses[0].queryResult;
--- main.js
+++ main.js
@@ -5,26 +5,65 @@ const server = http.createServer(app);
 const io = require('socket.io').listen(server);
 const ai = require('./api');
 const dialogFlow = ai.dialogFlow;
-// Authenticating on a global basis.
 
+const redis = require('redis');
+
+const client = redis.createClient();
+client.on('connect', function() {
+  console.log('connected');
+});
+
+app.use(express.static('./public'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.use(express.static('./public'));
+//Socket.IO
 
 io.on('connection', function(socket) {
 
-  socket.on('chat message', function(msg) {
-    io.emit('chat message', msg);
-    dialogFlow(msg).then((resp) => {
-      io.emit('chat message', resp);
+  new Promise((resolve, reject) => {
+
+    client.lrange('messagesList', 0, 10, function(err, data) {
+      if (err) {
+        reject();
+      } else {
+        console.log('The value of the "messagesList" ' + data);
+        resolve(data)
+      }
+    })
+  }).then((data) => {
+    data.forEach((message) => {
+      io.emit('chat message', message);
     });
   });
-  socket.on('disconnect', function() {
+  socket.on('chat message', function(msg) {
+
+    client.rpush("messagesList", msg, function(err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('the message key was successfully set', msg);
+    });
+
+    io.emit('chat message', msg);
+
+    dialogFlow(msg).then((resp) => {
+
+      client.rpush("messagesList", resp, function(err, data) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log('the response key was successfully set', resp);
+      });
+
+      io.emit('chat message', resp);
+    });
+
+    //When client disconnect show the value of the conv:
+    socket.on('disconnect', function() {});
     console.log('user disconnected');
   });
 });
-
 server.listen(process.env.PORT || 3000, () => console.log('listening on port 3000!'))
